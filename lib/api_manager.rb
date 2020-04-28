efce7f004f32c5d4860e90ee0c3684feaa5d263a
ChaoticBored <46114415+XenoQueen@users.noreply.github.com>
2020-04-28 11:18:17 -0700

    Name spaces CLI and APIManager files

---
+++ lib/api_manager.rb
@@ -0,0 +1,10 @@
+class APIManager
+
+  BASE_URL = "https://unknownworlds.com"
+
+  def self.get_games
+    url = BASE_URL + "games"
+    res = HTTParty.get(url)
+  end
+
+end
--- lib/uw_cli.rb
+++ lib/uw_cli.rb
@@ -1,5 +1,6 @@
 require_relative "./uw_cli/version"
 require_relative "./cli"
+require_relative "./api_manager"
 
 module UwCli
   class Error < StandardError; end
